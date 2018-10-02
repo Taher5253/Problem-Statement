0ffdfba3f75dbb1211e75a4a0dfaf5b5f3fb6f35
Taher Chittalwala <tchittalwala@gmail.com>
2018-10-03 05:18:46 +0530

    Update prob_stmt1.py

--- prob_stmt1.py
+++ prob_stmt1.py
@@ -1,29 +1,27 @@
 import urllib3
 import requests
 import json
-import pymongo
+import pymongo #importing pymongo library
 
-#pythonsg.ex('test')
 
+#Creating mongodb connection
 myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 
+#Creating database and collection object
 mydb = myclient["posts"]
 mycol = mydb["posts_collection"]
 
-print(mydb)
-print(mycol)
+#facebook url GET request
+url_request_facebook = requests.get("https://graph.facebook.com/v3.1/me?fields=posts.include_hidden(true).show_expired(true)%7Bfull_picture%2Clink%2Ccomments%2Cdescription%2Cwith_tags%2Cplace%2Cattachments%2Clikes%2Cicon%2Cpicture%2Ctype%7D&access_token=EAAd6TdjdprUBAC8Inew2WCZBznKDVDC58cDAZCYkyAZC1sZCdsRexIgxZCuAwT1FkmHNhgzbt8B9NCeYbbksTaXWCDD9lGgLY6ZBM0wLZCZCX5b1VvNOg6iMiESdToHIJIlvTgcJ3krv1SeM9iaQD5A2tPC2KbPYB70G8AO6DhAdBfZBLrbj9P6yy8v1hF0hmcdwhwN3cZA3Yf0gZDZD")
 
-attenders = requests.get("https://graph.facebook.com/v3.1/me?fields=posts.include_hidden(true).show_expired(true)%7Bfull_picture%2Clink%2Ccomments%2Cdescription%2Cwith_tags%2Cplace%2Cattachments%2Clikes%2Cicon%2Cpicture%2Ctype%7D&access_token=EAAd6TdjdprUBAC8Inew2WCZBznKDVDC58cDAZCYkyAZC1sZCdsRexIgxZCuAwT1FkmHNhgzbt8B9NCeYbbksTaXWCDD9lGgLY6ZBM0wLZCZCX5b1VvNOg6iMiESdToHIJIlvTgcJ3krv1SeM9iaQD5A2tPC2KbPYB70G8AO6DhAdBfZBLrbj9P6yy8v1hF0hmcdwhwN3cZA3Yf0gZDZD")
-attenders_json = attenders.json()
-json1_data = attenders_json['posts']['data']
-print(json1_data)
-#data  = json.loads(attenders_json)
+#creating json object
+data = url_request_facebook.json()
 
-mycol.insert_many(json1_data)
+dictionary_data = data['posts']['data']
 
-#y = json.loads(attenders_json)
 
-#print(attenders_json)
-print(type(attenders_json))
+#storing dictionary in database
+mycol.insert_many(dictionary_data)
+
 
 
